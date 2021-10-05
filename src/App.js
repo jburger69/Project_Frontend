48a65fa7b3841e885c1310f85c6a3cbe5fdcaeb3
Joshua Burger <joshberger6969@outlook.com>
2021-10-05 18:23:14 -0400

    Setup class component for top level App to render router with home and login page, Navbar component render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login';
+import Home from './components/Home';
+import Navbar from './components/NavBar';
 
 class App extends React.Component {
 
     render(){
-        return <h1>Hello World</h1>
+        return (
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path='/home' exact component={Home} />
+                    <Route path='/login' exact component={Login} />
+                </Switch>
+            </Router>
+        )
     }
 
 }
