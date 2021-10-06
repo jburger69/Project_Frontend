40db877206c4e7cc0831850c3aa0bbd290623c59
Joshua Burger <joshberger6969@outlook.com>
2021-10-06 18:52:26 -0400

    Constructor to take in currentuser and currentavatar, New profile route and logic for if user is not logged in then display log in page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,40 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './components/Login';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
+import Profile from './components/Profile';
 
 class App extends React.Component {
+
+
+    constructor() {
+        super()
+        this.state = {
+            CurrentUser: null,
+            CurrentAvatar: null
+        }
+    }
+
+    setCurrentUser = (data) => {
+        this.setState({
+            CurrentUser: data.user,
+            CurrentAvatar: data.avatar
+        })
+    }
 
     render(){
         return (
             <Router>
                 <Navbar />
                 <Switch>
-                    <Route path='/home' exact component={Home} />
-                    <Route path='/login' exact component={Login} />
+                    <Route path='/' exact component={Home} />
+                    <Route path='/login' render={(props) => <Login setCurrentUser={this.setCurrentUser} routerProps={props} />} />
+                    <Route path='/profile' render={() => {
+                        return this.state.CurrentUser ? (
+                            <Profile currentUser={this.state.CurrentUser} currentAvatar={this.state.CurrentAvatar} />
+                        ) : (
+                            <Login setCurrentUser={this.setCurrentUser} />
+                        )
+                    }} />
                 </Switch>
             </Router>
         )
