7c2a41bb01f327f30df4222ad2dc11700e1af4bb
Joshua Burger <joshberger6969@outlook.com>
2021-10-07 21:09:42 -0400

    Create and import CreateAccount component, New logout and pass props down through Navbar, Redirect if user is not logged in and tries to view profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Login from './components/Login';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
 import Profile from './components/Profile';
+import CreateAccount from './components/CreateAccount';
 
 class App extends React.Component {
 
@@ -23,10 +24,17 @@ class App extends React.Component {
         })
     }
 
+    logout = () => {
+        this.setState({
+            CurrentUser: null,
+            CurrentAvatar: null
+        })
+    }
+
     render(){
         return (
             <Router>
-                <Navbar />
+                <Navbar currentUser={this.state.CurrentUser} logout={this.logout}/>
                 <Switch>
                     <Route path='/' exact component={Home} />
                     <Route path='/login' render={(props) => <Login setCurrentUser={this.setCurrentUser} routerProps={props} />} />
@@ -34,9 +42,10 @@ class App extends React.Component {
                         return this.state.CurrentUser ? (
                             <Profile currentUser={this.state.CurrentUser} currentAvatar={this.state.CurrentAvatar} />
                         ) : (
-                            <Login setCurrentUser={this.setCurrentUser} />
+                            <Redirect to='/login' />
                         )
                     }} />
+                    <Route path='/create_account' component={CreateAccount} />
                 </Switch>
             </Router>
         )
