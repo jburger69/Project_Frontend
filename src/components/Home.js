319b56bb7de777a3008d56f72487ca6e4f59d786
Joshua Burger <joshberger6969@outlook.com>
2021-10-17 21:49:00 -0400

    Add more detail to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
 
     render(){
         return (
-            <h1>Welcome To My Project</h1>
+            <h1>Welcome To My Project this project is just for learning purposes for me to grow my coding skills!</h1>
         )
     }
 
