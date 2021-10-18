d680533987950fd8091fa271074f3da065576a0b
Joshua Burger <joshberger6969@outlook.com>
2021-10-17 21:49:40 -0400

    Remove old form and replace with new form and css

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,6 @@ class Login extends React.Component {
         }
     }
 
-    
     handleOnChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
@@ -40,25 +39,35 @@ class Login extends React.Component {
         })
     }
 
-
-
-
-
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>Name:</label>
-                <input type='text' name='name' value={this.state.name} onChange={this.handleOnChange}></input>
-                <label>Password:</label>
-                <input type='password' name='password' value={this.state.password} onChange={this.handleOnChange}></input>
-                <input type='submit' value={"Login"}></input>
+                <h3>Log In</h3>
+
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className="form-control" placeholder="Enter name" value={this.state.name} onChange={this.handleOnChange} />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" value={this.state.password} onChange={this.handleOnChange} />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block" value={"Login"}>Submit</button>
+                <p className="forgot-password text-right">
+                    Create Account <a href="/create_account">Here</a>
+                </p>
             </form>
         )
     }
-
-
-
-
 }
 
 export default Login;
