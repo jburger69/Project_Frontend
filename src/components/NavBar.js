b45b6843ca0c82b954497860a5b2160d063d3568
Joshua Burger <joshberger6969@outlook.com>
2021-10-17 21:50:28 -0400

    Remove old narbar and replce with new one and css, remove styled components no longer need.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,75 +2,75 @@ import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Nav = styled.nav`
-  background: #D67777;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.5rem calc((100vw - 1000px) / 2);
-  z-index: 10;
-  /* Third Nav */
-  /* justify-content: flex-start; */
-`;
+// export const Nav = styled.nav`
+//   background: #D67777;
+//   height: 80px;
+//   display: flex;
+//   justify-content: space-between;
+//   padding: 0.5rem calc((100vw - 1000px) / 2);
+//   z-index: 10;
+//   /* Third Nav */
+//   /* justify-content: flex-start; */
+// `;
 
-export const NavLink = styled(Link)`
-  color: #fff;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  // cursor: pointer;
-  // &.active {
-  //   color: #15cdfc;
-  // }
-`;
+// export const NavLink = styled(Link)`
+//   color: #fff;
+//   display: flex;
+//   align-items: center;
+//   text-decoration: none;
+//   padding: 0 1rem;
+//   height: 100%;
+//   // cursor: pointer;
+//   // &.active {
+//   //   color: #15cdfc;
+//   // }
+// `;
 
 
-export const NavMenu = styled.div`
-  display: flex;
-  align-items: center;
-  margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
+// export const NavMenu = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin-right: -24px;
+//   /* Second Nav */
+//   /* margin-right: 24px; */
+//   /* Third Nav */
+//   /* width: 100vw;
+//   white-space: nowrap; */
+//   @media screen and (max-width: 768px) {
+//     display: none;
+//   }
+// `;
 
-export const NavBtn = styled.nav`
-  display: flex;
-  align-items: center;
-  margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`;
+// export const NavBtn = styled.nav`
+//   display: flex;
+//   align-items: center;
+//   margin-right: 24px;
+//   /* Third Nav */
+//   /* justify-content: flex-end;
+//   width: 100vw; */
+//   @media screen and (max-width: 768px) {
+//     display: none;
+//   }
+// `;
 
-export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  background: #256ce1;
-  padding: 10px 22px;
-  color: #fff;
-  outline: none;
-  border: none;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  /* Second Nav */
-  margin-left: 24px;
-  &:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
-  }
-`;
+// export const NavBtnLink = styled(Link)`
+//   border-radius: 4px;
+//   background: #256ce1;
+//   padding: 10px 22px;
+//   color: #fff;
+//   outline: none;
+//   border: none;
+//   cursor: pointer;
+//   transition: all 0.2s ease-in-out;
+//   text-decoration: none;
+//   /* Second Nav */
+//   margin-left: 24px;
+//   &:hover {
+//     transition: all 0.2s ease-in-out;
+//     background: #fff;
+//     color: #010606;
+//   }
+// `;
 
 
 
@@ -81,31 +81,21 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <Nav>
-        <NavLink to='/'>
-        <img width="80px" height="auto" className="img-responsive"  alt="logo"  />
-        </NavLink>
-        <NavMenu>
-          <NavLink to='/'>
-            Home
-          </NavLink>
-          <NavLink to='/profile'>
-            profile
-          </NavLink>
-          {this.props.currentUser ? (
-            <NavLink to='/' onClick={this.props.logout} >
-            Logout
-            </NavLink>
-          ) : (
-            <NavLink to='/login'>
-            Login
-            </NavLink>
-          )}
-          <NavLink to='/create_account'>
-            Create an Account
-          </NavLink>
-        </NavMenu>
-      </Nav>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>BurgsBlogs</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/Login"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/create_account"}>Create Account</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   };
 }
